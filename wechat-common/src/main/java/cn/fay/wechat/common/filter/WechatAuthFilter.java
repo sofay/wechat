4d37838be255f30c97262f8ac722d50fcc0d39d4
Fay <nanyang.lf@raycloud.com>
2018-04-16 13:50:08 +0800

    fix auth bug

--- wechat-common/src/main/java/cn/fay/wechat/common/filter/WechatAuthFilter.java
+++ wechat-common/src/main/java/cn/fay/wechat/common/filter/WechatAuthFilter.java
@@ -3,6 +3,7 @@ package cn.fay.wechat.common.filter;
 import cn.fay.wechat.common.util.AppConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,9 @@ import java.util.Arrays;
  */
 public class WechatAuthFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(WechatAuthFilter.class);
+    private final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    @Value("wechat.config.token")
+    private String token;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -35,7 +39,7 @@ public class WechatAuthFilter implements Filter {
             String nonce = request.getParameter("nonce");
             String echostr = request.getParameter("echostr");
             LOGGER.info("wechat auth filter received:timestamp={}, nonce={}, echostr={}", timestamp, nonce, echostr);
-            String[] arr = new String[]{timestamp, nonce, echostr};
+            String[] arr = new String[]{timestamp, nonce, token};
             Arrays.sort(arr);
             StringBuilder str = new StringBuilder();
             for (String temp : arr) {
@@ -45,7 +49,7 @@ public class WechatAuthFilter implements Filter {
             try {
                 MessageDigest digest = MessageDigest.getInstance("sha1");
                 byte[] bytes = digest.digest(str.toString().getBytes(AppConstants.APP_ENCODING_NAME));
-                String encodeStr = new String(bytes, AppConstants.APP_ENCODING_NAME);
+                String encodeStr = getFormattedText(bytes);
                 LOGGER.info("wechat auth filter encode str:{}", encodeStr);
                 if (encodeStr.equals(signature)) {
                     response.getWriter().write(echostr);
@@ -61,5 +65,18 @@ public class WechatAuthFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+
+
+    private String getFormattedText(byte[] bytes) {
+        int len = bytes.length;
+        StringBuilder buf = new StringBuilder(len * 2);
+        // 把密文转换成十六进制的字符串形式
+        for (int j = 0; j < len; j++) {
+            buf.append(HEX_DIGITS[(bytes[j] >> 4) & 0x0f]);
+            buf.append(HEX_DIGITS[bytes[j] & 0x0f]);
+        }
+        return buf.toString();
     }
 }
